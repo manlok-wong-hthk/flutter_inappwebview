70d6a745cb50babd557a79e980331aae5950790e
Manlok Wong <manlok.wong@hthk.com>
2026-01-08 11:30:57 +0800

    add log

--- flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
+++ flutter_inappwebview_ios/ios/Classes/InAppWebView/InAppWebView.swift
@@ -1903,6 +1903,7 @@ public class InAppWebView: WKWebView, UIScrollViewDelegate, WKUIDelegate,
     public func webView(_ webView: WKWebView,
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            print("decidePolicyFor navigationAction: \(navigationAction.request.url?.absoluteString ?? "nil")")
         var decisionHandlerCalled = false
         let callback = WebViewChannelDelegate.ShouldOverrideUrlLoadingCallback()
         callback.nonNullSuccess = { (response: WKNavigationActionPolicy) in
